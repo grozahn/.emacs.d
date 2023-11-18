ccfd351cb7fa7c2fba07f5f69b91d5656c59c236
Andrey Streltsov <grozahn.labs@gmail.com>
2023-11-19 00:10:30 +0200

    [init.el] Made it less annoying

--- init.el
+++ init.el
@@ -8,3 +8,19 @@
   :unless (featurep 'early-init)
   :config
   (load-file (locate-user-emacs-file "early-init.el")))
+
+(use-package cus-edit
+  :custom
+  (custom-file (expand-file-name "custom.el" user-emacs-directory))
+  :init
+  (load custom-file :noerror))
+
+(use-package files
+  :custom
+  (make-backup-files nil)
+  (auto-save-default nil))
+
+(use-package ask-yn
+  :no-require t
+  :init
+  (fset 'yes-or-no-p 'y-or-n-p))
