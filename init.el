03aa0eadd460207415638445dcdffeb3721c05f1
Andrey Streltsov <grozahn.labs@gmail.com>
2023-11-19 00:24:46 +0200

    [init.el] Vital plugins added

--- init.el
+++ init.el
@@ -24,3 +24,48 @@
   :no-require t
   :init
   (fset 'yes-or-no-p 'y-or-n-p))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package which-key
+  :ensure t
+  :hook (after-init . which-key-mode))
+
+(use-package vterm
+  :ensure t
+  :when (bound-and-true-p module-file-suffix)
+  :requires project
+  :custom
+  (vterm-always-compile-module t)
+  (vterm-environment '("VTERM=1"))
+  (vterm-max-scrollback 1000)
+  (vterm-kill-buffer-on-exit t))
+
+(use-package company
+  :ensure t
+  :bind (:map
+         company-active-map
+         ("TAB" . company-complete-common-or-cycle)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<S-Tab>" . company-select-previous)
+         ("<backtab>" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :hook (prog-mode . company-mode)
+  :custom
+  (company-idle-delay 0.2)
+  (company-require-match 'never)
+  (company-minimum-prefix-length 2)
+  (company-tooltip-align-annotations nil))
